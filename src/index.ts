8caccadbd042c15eb35eeb8712cf0d8af5f267c4
Muhammad Hamza <hamza@sintov.com>
2024-04-03 16:42:45 +0500

    index.ts origin updated

--- src/index.ts
+++ src/index.ts
@@ -27,7 +27,11 @@ dotenv.config();
  */
 const app: Express = express();
 const port = process.env.PORT || 3000;
-app.use(cors());
+app.use(cors({
+  origin: 'http://44.235.84.229:3000',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  credentials: true,
+}));
 app.use(traceMiddleware);
 app.use(errorHandlerMiddleware);
 app.use(logRequest);
